eb9acc410599694e71f4b205fb5df2c2409e7f36
FELIPE RODRIGUES DE MELO <85718153+Felipe00007@users.noreply.github.com>
2021-06-28 14:49:47 -0300

    av.js

---
+++ av1.js
@@ -0,0 +1,28 @@
+function menu(prato, tempo) {
+
+    let pipoca = 10;
+    let macarrao = 8;
+    let carne = 15;
+    let feijao = 12;
+    let brigadeiro = 8;
+
+    lógica...
+
+    switch (valor) {
+        case valorA:
+            console.log('');
+            break;
+
+        case (valorB):
+            console.log('');
+            break;
+
+        case (valorC):
+            console.log('');
+            break;
+
+        default:
+            console.log('');
+    }
+
+}
